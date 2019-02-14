3d2dc7590fae6c498eb472606f06b9e265f41587
Jessica Chen <jchen@dhcp-10-101-136-235.wireless.northwestern.private>
2019-02-13 19:18:08 -0600

    concert and movie axios call works

--- liri.js
+++ liri.js
@@ -8,17 +8,20 @@ var axios = require("axios");
 
 var searchThis = process.argv[2];
 
-var searchName = process.argv[3];
+var movie, artist= process.argv[3];
 
-var movieUrl = "http://www.omdbapi.com/?t=" + searchName + "&y=&plot=short&apikey=trilogy";
+movie = artist
+
+var artist = process.argv[3];
+
+var movieUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
 var musicUrl ;
 
-var concertUrl = "https://rest.bandsintown.com/artists/" + searchName + "/events?app_id=codingbootcamp";
+var concertUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-function getMovie(){
+
 axios.get(movieUrl)
-
 .then(
     function(movieData){
         if (searchThis === "movie-this") {
@@ -27,26 +30,26 @@ axios.get(movieUrl)
         console.log("Title: " + movieData.data.Title);
         console.log("Year: " + movieData.data.Year);
         console.log("imdbRating: " + movieData.data.imdbRating);
-        console.log(movieData.data.Ratings[1]);
+        console.log("Rotten Tomatoes Rating: " + movieData.data.Ratings[1]);
         console.log("Country: " + movieData.data.Country);
         console.log("Plot: " + movieData.data.Plot);
         console.log("Language: " + movieData.data.Language);
         console.log("Actors: " + movieData.data.Actors);
         }
-    })
-};
-
-function getConcert() {
+    });
 
 axios.get(concertUrl)
 .then(
     function(concertData){
         if (searchThis === "concert-this") {
-            console.log(concertData.data);
+            //console.log(concertData.data);
+
+            console.log("Venue: " + concertData.data[1].venue.name);
+            console.log("City: " + concertData.data[1].venue.city);
+            console.log("Region: " + concertData.data[1].venue.region);
+            console.log("Country: " + concertData.data[1].venue.country);
+            console.log("Date of Event: " + concertData.data[1].datetime);
+
         }
 
-    })
-};
-
-getMovie();
-//getConcert();
+    });
