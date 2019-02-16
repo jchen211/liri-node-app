64bb74bace962a766ad12697841dc4b767050eb0
Jessica Chen <jchen@jchen.attlocal.net>
2019-02-16 15:56:27 -0600

    another liri.js file to test switch/cases

--- liri.js
+++ liri.js
@@ -1,55 +1,42 @@
+// spotify 
 require ("dotenv").config();
-
+var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
+var spotifySongs = new Spotify(keys.spotify);
 
-//var spotify = new spotify(keys.spotify);
-
+// file share
+var fs = require("fs");
+// axios
 var axios = require("axios");
 
 var searchThis = process.argv[2];
+var movie, artist, song = process.argv[3];
+    movie = artist = song
 
-var movie, artist= process.argv[3];
+function callLiri() {
+    switch (searchThis) {
+        case "movie-this":
 
-movie = artist
+        if (!movie) {
+            movie = "Mr.Nobody";
+        }
 
-var artist = process.argv[3];
+        var movieUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-var movieUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-var musicUrl ;
-
-var concertUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-
-axios.get(movieUrl)
-.then(
-    function(movieData){
-        if (searchThis === "movie-this") {
-        // console.log(movieData.data);
+        axios.get(movieUrl)
+        .then(function(movieData){
+            console.log("Title: " + movieData.data.Title);
+            console.log("Year: " + movieData.data.Year);
+            console.log("imdbRating: " + movieData.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + movieData.data.Ratings[1].Value);
+            console.log("Country: " + movieData.data.Country);
+            console.log("Plot: " + movieData.data.Plot);
+            console.log("Language: " + movieData.data.Language);
+            console.log("Actors: " + movieData.data.Actors);
         
-        console.log("Title: " + movieData.data.Title);
-        console.log("Year: " + movieData.data.Year);
-        console.log("imdbRating: " + movieData.data.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + movieData.data.Ratings[1]);
-        console.log("Country: " + movieData.data.Country);
-        console.log("Plot: " + movieData.data.Plot);
-        console.log("Language: " + movieData.data.Language);
-        console.log("Actors: " + movieData.data.Actors);
-        }
-    });
+        
+        });
+    }
+}
 
-axios.get(concertUrl)
-.then(
-    function(concertData){
-        if (searchThis === "concert-this") {
-            //console.log(concertData.data);
-
-            console.log("Venue: " + concertData.data[1].venue.name);
-            console.log("City: " + concertData.data[1].venue.city);
-            console.log("Region: " + concertData.data[1].venue.region);
-            console.log("Country: " + concertData.data[1].venue.country);
-            console.log("Date of Event: " + concertData.data[1].datetime);
-
-        }
-
-    });
+callLiri();
---
+++ liri2.js
@@ -0,0 +1,74 @@
+// spotify 
+require ("dotenv").config();
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+var spotifySongs = new Spotify(keys.spotify);
+
+// file share
+var fs = require("fs");
+// axios
+var axios = require("axios");
+
+
+var searchThis = process.argv[2];
+var movie, artist, song = process.argv[3];
+    movie = artist = song
+
+var movieUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+var concertUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+
+axios.get(movieUrl)
+.then(
+    function(movieData){
+        if (!movie) {
+            movie = "Mr.Nobody";
+        }
+
+        if (searchThis === "movie-this") {  
+        // console.log(movieData.data);
+        
+        console.log("Title: " + movieData.data.Title);
+        console.log("Year: " + movieData.data.Year);
+        console.log("imdbRating: " + movieData.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + movieData.data.Ratings[1].Value);
+        console.log("Country: " + movieData.data.Country);
+        console.log("Plot: " + movieData.data.Plot);
+        console.log("Language: " + movieData.data.Language);
+        console.log("Actors: " + movieData.data.Actors);
+        }
+    
+});
+
+axios.get(concertUrl)
+.then(
+    function(concertData){
+        if (searchThis === "concert-this") {
+            //console.log(concertData.data);
+
+            console.log("Venue: " + concertData.data[1].venue.name);
+            console.log("City: " + concertData.data[1].venue.city);
+            console.log("Region: " + concertData.data[1].venue.region);
+            console.log("Country: " + concertData.data[1].venue.country);
+            console.log("Date of Event: " + concertData.data[1].datetime);
+        }
+    });
+/* 
+spotifySongs.search({
+        type: "track",
+        query: song
+        }, function(err, musicData) {
+            if (err) {
+                return console.log("Error: " + err);
+
+            } if (searchThis === "spotify-this-song") {
+                console.log(musicData.tracks.items[2].album.artists.type);
+            }
+        });
+            
+     */
+fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+});
